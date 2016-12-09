dfb86d1c02aacf5d2d4d4e1ab813cca3c0f6ea93
petitjhn <petitjhn@gmail.com>
2016-12-09 15:59:25 +0300

    Update and rename reconstruction_2.py to Unsup.py

--- Unsup.py
+++ Unsup.py
@@ -1,20 +1,15 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Dec  9 00:00:51 2016
-
-@author: root
-"""
-
+#import dependancies
 import pandas as pd
+#loading the training data
 train=pd.read_csv('train.csv',delimiter =',',skiprows=1)
-     
+ #slicing off the labels    
 X=train.values[:,1:]
 Y=train.values[:,0]
-
+#importing of our classifier
 from sklearn.cluster import KMeans
 classifier=KMeans(init='k-means++',n_clusters=10)
+#training of classifier on dataset
 classifier.fit(X,Y)
-
+#testing of classifier on test dataset
 test=pd.read_csv('test.csv')
 classifier.predict(test)
-print classifier.score(X,Y)
